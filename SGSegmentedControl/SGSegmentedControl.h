bf1314e5672092eb7fc5d97511de2b9f9240f4ea
kingsic <kingsic@126.com>
2016-11-22 18:53:54 +0800

    增加 SGSegmentedControl 使用说明

--- SGSegmentedControl/SGSegmentedControl.h
+++ SGSegmentedControl/SGSegmentedControl.h
@@ -13,11 +13,16 @@
 //  - - GitHub下载地址 https://github.com/kingsic/SGSegmentedControl.git - - - //
 //
 //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+//
+// SGSegmentedControl 使用说明：
+// SGSegmentedControlStatic 类：顶部标题按钮的个数比较少(或已经明确个数)且标题按钮的总个数宽度并没有超出屏幕尺寸
+// SGSegmentedControlDefault 类：顶部标题按钮的个数比较多且标题按钮的总个数宽度已经超出屏幕尺寸
+// SGSegmentedControlDefault 这个类的指示器样式多一点而 SGSegmentedControlStatic 这个类里面并没有做处理(请根据自己的项目需求，参考 SGSegmentedControlDefault 这个类进行相应的修改)；
+// 注意：指示器样式三是使用 UIImageView 创建的，指示器颜色属性(indicatorColor)对此并不起作用
 
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import "SGSegmentedControlStatic.h"
 #import "SGSegmentedControlDefault.h"
 #import "SGSegmentedControlBottomView.h"
-
 
