32d52dea1ead5aa03b7e292ed5a7f85b31a41570
kingsic <kingsic@126.com>
2016-12-15 15:11:53 +0800

    删除不必要的代码

--- SGSegmentedControlExample/ViewController.m
+++ SGSegmentedControlExample/ViewController.m
@@ -11,7 +11,6 @@
 #import "NewStyleTwoVC.h"
 #import "NewStyleThreeVC.h"
 #import "StyleTwoVC.h"
-#import "StyleThreeVC.h"
 #import "StyleFourVC.h"
 #import "StyleFiveVC.h"
 #import "StyleSixVC.h"
@@ -34,14 +33,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.title_arr = @[@"静止状态下标题按钮样式一", @"静止状态下标题按钮样式二", @"静止状态下标题按钮样式三", @"滚动状态下标题按钮", @"静态状态下带有图片的标题按钮", @"滚动状态下带有图片的标题按钮", @"指示器样式一", @"指示器样式二", @"指示器样式三", @"标题按钮文字渐显效果", @"标题按钮文字缩放效果", @"Block创建滚动状态下标题按钮", @"导航栏上面的标题按钮"];
+    self.title_arr = @[@"静止状态下标题按钮样式一", @"静止状态下标题按钮样式二", @"静止状态下标题按钮样式三", @"滚动状态下标题按钮", @"滚动状态下带有图片的标题按钮", @"指示器样式一", @"指示器样式二", @"指示器样式三", @"标题按钮文字渐显效果", @"标题按钮文字缩放效果", @"Block创建滚动状态下标题按钮", @"导航栏上面的标题按钮"];
     
     NewStyleOneVC *newOneVC = [[NewStyleOneVC alloc] init];
     NewStyleTwoVC *newTwoVC = [[NewStyleTwoVC alloc] init];
     NewStyleThreeVC *newThreeVC = [[NewStyleThreeVC alloc] init];
     
     StyleTwoVC *twoVC = [[StyleTwoVC alloc] init];
-    StyleThreeVC *threeVC = [[StyleThreeVC alloc] init];
     StyleFourVC *fourVC = [[StyleFourVC alloc] init];
     StyleFiveVC *fiveVC = [[StyleFiveVC alloc] init];
     StyleSixVC *sixVC = [[StyleSixVC alloc] init];
@@ -51,7 +49,7 @@
     StyleBlockVC *blockVC = [[StyleBlockVC alloc] init];
     StyleLastVC *lastVC = [[StyleLastVC alloc] init];
 
-    self.VC_arr = @[newOneVC, newTwoVC, newThreeVC, twoVC, threeVC, fourVC, fiveVC, sixVC, sevenVC, eightVC, nineVC, blockVC, lastVC];
+    self.VC_arr = @[newOneVC, newTwoVC, newThreeVC, twoVC, fourVC, fiveVC, sixVC, sevenVC, eightVC, nineVC, blockVC, lastVC];
 
     // 注册
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
