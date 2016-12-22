276c9bcb83d30c812c73543a99eecd271d290a33
kingsic <kingsic@126.com>
2016-12-22 18:16:37 +0800

    添加提示内容

--- SGSegmentedControl/SGSegmentedControlHelper.h
+++ SGSegmentedControl/SGSegmentedControlHelper.h
@@ -10,6 +10,12 @@
 #ifndef SGSegmentedControlHelper_h
 #define SGSegmentedControlHelper_h
 
+/*
+    使用说明
+        建议（可不采取）
+        创建底部滚动视图，最好底部视图的高度等于整个屏幕的高度，如是里面的子控制器里面创建UITableView，建议修改UITableView的contentInset属性
+ */
+
 #import "UIView+SGExtension.h"
 
 /** 指示器默认颜色 */
@@ -31,6 +37,7 @@ static CGFloat SG_btnMargin = 15;
 static CGFloat SG_btnScale = 0.14;
 /** 默认SGImageButton_Horizontal图片的宽度 */
 static CGFloat SG_defaultHorizontalImageWidth = 20;
+
 
 
 
