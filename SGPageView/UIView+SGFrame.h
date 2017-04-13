c8dd9a2dc4399b4b7d7e2f15da1156026ab60bde
kingsic <kingsic@126.com>
2017-04-13 17:44:11 +0800

    版本升级

--- SGPageView/UIView+SGFrame.h
+++ SGPageView/UIView+SGFrame.h
@@ -12,7 +12,7 @@
 //
 //  - - 如在使用中, 遇到什么问题或者有更好建议者, 请于 kingsic@126.com 邮箱联系 - - - //
 //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//  - - GitHub下载地址 https://github.com/kingsic/SGSegmentedControl.git - - - //
+//  - - GitHub下载地址 https://github.com/kingsic/SGPageView.git - - - - - - - //
 //
 //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
